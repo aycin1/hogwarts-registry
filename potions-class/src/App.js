0610fefdc7d17e626d01c67ea6286c2d9a5a3bf9
Aycin Kaya <aycin@Aycins-MBP-2.broadband>
2023-06-20 15:11:54 +0100

    Initial template from SigmaLabs

---
+++ potions-class/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import React, { useState, useEffect } from 'react'
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="title">Potions Class</header>
+      <div className="app-wrapper">
+        <h1>Insert code here!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
